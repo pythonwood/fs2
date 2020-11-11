e632dcee12835f659f7b9cf534c0aedbf9a95a31
pythonwood <582223837@qq.com>
2020-11-11 09:54:17 +0800

    support muti fs urls. for loop in group is better than for loop in subcmd.

--- fs/commands/fs2.py
+++ fs/commands/fs2.py
@@ -1,6 +1,7 @@
 from fs import open_fs, errors
 import shutil
 import click
+from collections import OrderedDict
 
 def _listopener():
     from fs.opener  import registry
@@ -8,9 +9,17 @@ def _listopener():
     for opener in openers:
         print(str(opener), 'for', ['%s://' % i for i in opener.protocols])
 
+def _open_fs(url):
+    if '://' not in url:
+        url = 'file://' + url
+    _fs = open_fs(url)
+    setattr(_fs, 'url', url)
+    return _fs
+
+
 @click.group(invoke_without_command=True) #https://click.palletsprojects.com/en/7.x/commands/#group-invocation-without-command
 @click.option('--listopener', '-l', is_flag=True, help='list supported file system')
-@click.option('--url', '-u', default='.', help='filesystem url: default is "."')
+@click.option('--url', '-u', multiple=True, default='.', help='filesystem url: default is "."')
 @click.pass_context
 def fs2(ctx, listopener, url):
     '''This script is pyfilesystem2 command line tool.
@@ -25,18 +34,30 @@ def fs2(ctx, listopener, url):
         fs2 -u s3:// ls .                                 # pip install fs-s3fs
         fs2 -u dropbox:// ls .                            # pip install fs.dropboxfs
         fs2 -u webdav://user:pass@127.0.0.1/web/dav/ ls . # pip install fs.webdavfs
-        fs2 -u ssh:// ls .                                # pip install fs.sshfs
+        fs2 -u ssh://my.vps.com/home/ ls .                                # pip install fs.sshfs
         fs2 --listopener                                  # list all support filesystem
+
+    \b
+    mutil url is supported:
+        fs2 -u /tmp -u ssh://vps/tmp ls .
+
     '''
     # print(vars(ctx), listopener, url)
     if listopener:
         _listopener()
-        return
+        ctx.exit()
     if not (ctx.args or ctx.invoked_subcommand):
         click.echo(ctx.get_help())
-    if '://' not in url:
-        url = 'file://' + url
+        ctx.exit()
+
     ctx.ensure_object(dict)
-    ctx.obj['url'] = url
-    ctx.obj['fs'] = open_fs(url)
+    # so, a for loop in group is better than for loop in subcmd
+    # # ctx.obj['fss'] = OrderedDict((u, _open_fs(u)) for u in url)
+    for u in url:
+        ctx.obj['url'] = u
+        ctx.obj['fs'] = _open_fs(u)
+        subcmd = click.Group.get_command(fs2, ctx, ctx.invoked_subcommand)
+        ctx.invoke(subcmd)
+    ctx.exit()
+
 
